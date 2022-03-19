bfd1e793bba1eb423d81b470e19cb7f7ed0bc298
kemalakoglu <kemalakoglu@outlook.com>
2022-03-19 18:51:48 +0300

    Unit tests are written

---
+++ test/link.shortener.dashboard.controller.test.js
@@ -0,0 +1,91 @@
+const assert = require('assert');
+const LinkModel = require("../src/02-Domain/aggregates/link.shortener.model");
+
+describe('Get Method', function () {
+    describe('If Requested Id is exist', function () {
+        it('should return Link Model', function () {
+            assert.ok(new LinkModel());
+        });
+    });
+
+    describe('If Requested Id is not exist', function () {
+        it('should return empty model message', function () {
+            assert.ok('Link is not exist');
+        });
+    });
+
+    describe('If request has no Id parameter', function () {
+        it('should return error message', function () {
+            assert.fail('Id parameter is required');
+        });
+    });
+});
+
+describe('Create Method', function () {
+    describe('If Request Model is valid', function () {
+        it('should return true', function () {
+            assert.ok(true);
+        });
+    });
+
+    describe('If Request Model is not valid', function () {
+        it('should return not valid model message', function () {
+            assert.ok('model is not valid');
+        });
+    });
+});
+
+describe('Update Method', function () {
+    describe('If Request Model is valid', function () {
+        it('should return true', function () {
+            assert.ok(true);
+        });
+    });
+
+    describe('If Request Model is not valid', function () {
+        it('should return not valid model message', function () {
+            assert.ok('model is not valid');
+        });
+    });
+});
+
+describe('Delete Method', function () {
+    describe('If Requested Id is exist', function () {
+        it('should return true', function () {
+            assert.ok(true);
+        });
+    });
+
+    describe('If Requested Id is not exist', function () {
+        it('should return empty model message', function () {
+            assert.ok('Link is not exist');
+        });
+    });
+
+    describe('If request has no Id parameter', function () {
+        it('should return error message', function () {
+            assert.fail('Id parameter is required');
+        });
+    });
+});
+
+describe('List by Page Method', function () {
+    describe('If Requested Page Size and Number is valid', function () {
+        describe('If Result is not null', function () {
+            it('should return Link[]', function () {
+                assert.ok(new LinkModel[new LinkModel()]);
+            });
+        });
+        describe('If Result is null', function () {
+            it('should return empty message', function () {
+                assert.ok('Data not found');
+            });
+        });
+    });
+
+    describe('If Requested Page Size and Number is not valid', function () {
+        it('should return error message', function () {
+            assert.ok('Page Size or Number is not valid');
+        });
+    });
+});
